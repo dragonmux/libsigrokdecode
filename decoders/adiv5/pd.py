7d28a6724e2074e14866ae44d0900acddfb5fd96
dragonmux <git@dragonmux.network>
2024-02-13 21:40:09 +0000

    adiv5: Implemented odd/even display of transactions to make reading the trace easier

--- decoders/adiv5/pd.py
+++ decoders/adiv5/pd.py
@@ -22,6 +22,13 @@ from typing import Literal
 
 __all__ = ['Decoder']
 
+class Annotations:
+	'''Annotation and binary output classes.'''
+	(
+		TRANS_EVEN, TRANS_ODD,
+	) = range(2)
+A = Annotations
+
 ADIv5Op = Literal['DP_READ', 'DP_WRITE', 'AP_READ', 'AP_WRITE']
 ADIv5Ack = Literal['OK', 'WAIT', 'FAULT', 'NO-RESPONSE']
 
@@ -36,18 +43,37 @@ class Decoder(srd.Decoder):
 	outputs: list[str] = []
 	tags = ['Debug/trace']
 	annotations = (
+		('transaction-even', 'Transaction (even)'),
+		('transaction-odd', 'Transaction (odd)'),
 	)
 	annotation_rows = (
+		('transaction-even', 'Transaction (even)', (A.TRANS_EVEN,)),
+		('transaction-odd', 'Transaction (odd)', (A.TRANS_ODD,)),
 	)
 
 	def __init__(self):
 		pass
 
 	def reset(self):
-		pass
+		self.transactCount = 0
 
 	def start(self):
 		self.reset()
+		self.outputAnnotation = self.register(srd.OUTPUT_ANN)
 
-	def decode(self, beginSample: int, endSample: int, data: tuple[ADIv5Op, int, int, str, ADIv5Ack, int]):
-		pass
+	def annotate(self, begin: int, end: int, data: list[int | list[str]]):
+		self.put(begin, end, self.outputAnnotation, data)
+
+	def decode(self, beginSample: int, endSample: int, transact: tuple[ADIv5Op, int, int, str, ADIv5Ack, int]):
+		'''Take a transaction from the ADIv5 de-encapsulation decoder and turn it into part of a logical transaction'''
+		# Unpack the transaction
+		op, dp, addr, reg, ack, data = transact
+		target, op = op.split('_')
+
+		# Figure out which transaction line to display it and convert the transaction into an annotation
+		line = A.TRANS_EVEN if (self.transactCount & 1) == 0 else A.TRANS_ODD
+		self.transactCount += 1
+		targetName = f'DP{dp}'
+		if target == 'AP':
+			targetName += ' AP'
+		self.annotate(beginSample, endSample, [line, [f'{targetName} {op.lower()} {reg}: {data:08x}']])
