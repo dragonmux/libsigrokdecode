57edae8a0c0bf5087bc1bd19c7b3195eb4338c15
dragonmux <git@dragonmux.network>
2024-02-15 05:18:16 +0000

    adiv5: Built out AP/DP decode machinary to be able to track state to rebuild high level transactions from

--- decoders/adiv5/pd.py
+++ decoders/adiv5/pd.py
@@ -18,6 +18,7 @@
 ##
 
 import sigrokdecode as srd
+from enum import Enum, unique, auto
 from typing import Literal
 
 __all__ = ['Decoder']
@@ -31,6 +32,112 @@ A = Annotations
 
 ADIv5Op = Literal['DP_READ', 'DP_WRITE', 'AP_READ', 'AP_WRITE']
 ADIv5Ack = Literal['OK', 'WAIT', 'FAULT', 'NO-RESPONSE']
+
+@unique
+class ADIv5Target(Enum):
+	ap = auto()
+	dp = auto()
+
+@unique
+class ADIv5RnW(Enum):
+	write = auto()
+	read = auto()
+
+@unique
+class ADIv5Ack(Enum):
+	ok = auto()
+	wait = auto()
+	fault = auto()
+	noResult = auto()
+
+class ADIv5Transaction:
+	def __init__(self, op: ADIv5Op, dp: int, addr: int, reg: str, ack: ADIv5Ack, data: int):
+		target, rnw = op.split('_')
+		self.target = ADIv5Target.ap if target == 'AP' else ADIv5Target.dp
+		self.rnw = ADIv5RnW.read if rnw == 'READ' else ADIv5RnW.write
+		self.dp = dp
+		self.register = addr, reg
+		self.data = data
+
+		if ack == 'OK':
+			self.ack = ADIv5Ack.ok
+		elif ack == 'WAIT':
+			self.ack = ADIv5Ack.wait
+		elif ack == 'FAULT':
+			self.ack = ADIv5Ack.fault
+		elif ack == 'NO-RESPONSE':
+			self.ack = ADIv5Ack.noResult
+		else:
+			raise ValueError('Invalid ACK value given')
+
+class ADIv5AP:
+	'''This serves as a base type for all AP variants'''
+	@staticmethod
+	def fromID(id: int):
+		'''Construct a suitable AP instance from an ID register value'''
+		pass
+
+class ADIv5DPCtrlStat:
+	pass
+
+class ADIv5DPID:
+	pass
+
+class ADIv5DPTargetID:
+	pass
+
+class ADIv5DPSelect:
+	'''Internal representation of the state of the DP SELECT register (NB, we only care about the AP selected)'''
+	def __init__(self):
+		self.currentAP = 0
+
+	def changeValue(self, select: int):
+		'''Decode a write to the SELECT register to get the new value'''
+		self.currentAP = select >> 24
+
+class ADIv5DP:
+	def __init__(self):
+		self.abort = 0
+		self.ctrlstat = 0
+		self.select = ADIv5DPSelect()
+		self.rdbuff = 0
+		self.dpidr = 0
+		self.dlcr = 0
+		self.targetid = 0
+		self.dlpidr = 0
+		self.eventstat = 0
+		self.ap: dict[int, ADIv5AP] = {}
+
+	def decodeTransaction(self, transaction: ADIv5Transaction):
+		# If the transaction is for the DP, process the data into current register state
+		if transaction.target == ADIv5Target.dp:
+			reg = transaction.register[1]
+			if reg == 'ABORT':
+				self.abort = transaction.data
+			elif reg == 'CTRL/STAT':
+				self.ctrlstat = transaction.data
+			elif reg == 'SELECT':
+				self.select.changeValue(transaction.data)
+			elif reg == 'RDBUFF':
+				self.rdbuff = transaction.data
+			elif reg == 'DPIDR':
+				self.dpidr = transaction.data
+			elif reg == 'DLCR':
+				self.dlcr = transaction.data
+			elif reg == 'TARGETID':
+				self.targetid = transaction.data
+			elif reg == 'DLPIDR':
+				self.dlpidr = transaction.data
+			elif reg == 'EVENTSTAT':
+				self.eventstat = transaction.data
+			else:
+				raise ValueError(f'Invalid DP register {reg} given')
+		else:
+			# If the DP for this transaction is not yet known, see if this is an AP IDR transaction
+			# and if so, make a new DP instance based on the decoded value
+			ap = self.ap.get(self.select.currentAP)
+			if ap is None:
+				return
 
 class Decoder(srd.Decoder):
 	api_version = 3
@@ -52,10 +159,11 @@ class Decoder(srd.Decoder):
 	)
 
 	def __init__(self):
-		pass
+		self.dp: dict[int, ADIv5DP] = {}
 
 	def reset(self):
 		self.transactCount = 0
+		self.dp.clear()
 
 	def start(self):
 		self.reset()
